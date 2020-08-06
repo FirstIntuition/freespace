a0ace2d95b940f3ff09f65f0bcbb4f1312ce539d
Deep <deepmumbai16@gmail.com>
2020-08-06 13:59:07 +0530

    Added Basic Autocomplete functionality.

--- Public/JavaScript files/homepagejs.js
+++ Public/JavaScript files/homepagejs.js
@@ -15,3 +15,26 @@ $("#searchbutton").on("click",function(){
   $("#searchbutton").addClass("yearbtnpressed");
   setTimeout(function(){$("#searchbutton").removeClass("yearbtnpressed");},400);
 });
+//AutoComplete
+$(document).ready(function(){
+  // Defining the local dataset
+  var tagHelper = ['2020', '2019', '2018', '2017', '2016', '2015', 'Maths', 'Communication Skills', 'DataBase Management Systems', 'Computer Architecture and Organization', 'Abstract Data Structures', 'Physics', 'Chemistry', 'EVS'];
+  
+  // Constructing the suggestion engine
+  var tagHelper = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.whitespace,
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      local: tagHelper
+  });
+  
+  // Initializing the typeahead
+  $('#searchBar').typeahead({
+      hint: true,
+      highlight: true, /* Enable substring highlighting */
+      minLength: 1 /* Specify minimum characters required for showing suggestions */
+  },
+  {
+      name: 'tagHelper',
+      source: tagHelper
+  });
+});
