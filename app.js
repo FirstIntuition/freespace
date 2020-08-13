2d41e66cd81224768472707f54952790cd627eca
sahil27301 <65967824+sahil27301@users.noreply.github.com>
2020-08-13 15:54:32 +0530

    Fixed query construction

--- app.js
+++ app.js
@@ -16,104 +16,81 @@ app.get("/", function(req, res){
 
 app.use(bodyParser.json());
 app.post("/search", function(req, res){
-  // console.log(typeofreq.body);
-  // console.log(typeof obj.subjects);
-  console.log(req.body);
   var obj=req.body;
   var query='select * from documents, topics';
   var entered = false;
-  if (typeof obj.year != 'undefined') {
+  if (obj.year.length) {
     if (!entered) {
       query += ' where (';
     }else {
       query += ' and (';
     }
     entered=true;
-    if (typeof obj.year == 'string') {
-      query += "doc_date_asked = '"+obj.year+"'";
-    } else {
-      for (var i = 0; i < obj.year.length; i++) {
-        if (i) {
-          query += ' or ';
-        }
-        query +=  "doc_date_asked = '" + obj.year[i] + "'";
+    for (var i = 0; i < obj.year.length; i++) {
+      if (i) {
+        query += ' or ';
       }
+      query +=  "doc_date_asked = '" + obj.year[i] + "'";
     }
     query += ')';
   }
-  if (typeof obj.collegeYears != 'undefined') {
+  if (obj.collegeYears.length) {
     if (!entered) {
       query += ' where (';
     }else {
       query += ' and (';
     }
     entered=true;
-    if (typeof obj.collegeYears == 'string') {
-      query += "doc_year = '"+obj.collegeYears+"'";
-    } else {
-      for (var i = 0; i < obj.collegeYears.length; i++) {
-        if (i) {
-          query += ' or ';
-        }
-        query +=  "doc_year = '" + obj.collegeYears[i] + "'";
+    for (var i = 0; i < obj.collegeYears.length; i++) {
+      if (i) {
+        query += ' or ';
       }
+      query +=  "doc_year = '" + obj.collegeYears[i] + "'";
     }
     query += ')';
   }
-  if (typeof obj.examType != 'undefined') {
+  if (obj.examType.length) {
     if (!entered) {
       query += ' where (';
     }else {
       query += ' and (';
     }
     entered=true;
-    if (typeof obj.examType == 'string') {
-      query += "doc_exam = '"+obj.examType+"'";
-    } else {
-      for (var i = 0; i < obj.examType.length; i++) {
-        if (i) {
-          query += ' or ';
-        }
-        query +=  "doc_exam = '" + obj.examType[i] + "'";
+    for (var i = 0; i < obj.examType.length; i++) {
+      if (i) {
+        query += ' or ';
       }
+      query +=  "doc_exam = '" + obj.examType[i] + "'";
     }
     query += ')';
   }
-  if (typeof obj.topics != 'undefined') {
+  if (obj.topics.length) {
     if (!entered) {
       query += ' where (';
     }else {
       query += ' and (';
     }
     entered=true;
-    if (typeof obj.topics == 'string') {
-      query += "topic = '"+obj.topics+"'";
-    } else {
-      for (var i = 0; i < obj.topics.length; i++) {
-        if (i) {
-          query += ' or ';
-        }
-        query +=  "topic = '" + obj.topics[i] + "'";
+    for (var i = 0; i < obj.topics.length; i++) {
+      if (i) {
+        query += ' or ';
       }
+      query +=  "topic = '" + obj.topics[i] + "'";
     }
     query += ')';
   }
-  if (typeof obj.subjects != 'undefined') {
+  if (obj.subjects.length) {
     if (!entered) {
       query += ' where (';
     }else {
       query += ' and (';
     }
     entered=true;
-    if (typeof obj.subjects == 'string') {
-      query += "doc_subject = '"+obj.subjects+"'";
-    } else {
-      for (var i = 0; i < obj.subjects.length; i++) {
-        if (i) {
-          query += ' or ';
-        }
-        query +=  "doc_subject = '" + obj.subjects[i] + "'";
+    for (var i = 0; i < obj.subjects.length; i++) {
+      if (i) {
+        query += ' or ';
       }
+      query +=  "doc_subject = '" + obj.subjects[i] + "'";
     }
     query += ')';
   }
@@ -134,6 +111,7 @@ app.post("/search", function(req, res){
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+    console.log(query);
     con.query(query, function (err, result, fields) {
     if (err) throw err;
     console.log(result);
